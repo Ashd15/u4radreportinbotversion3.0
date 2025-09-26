0aa71bba4193ecc9169c32203de835a2b817ad77
rjangid631 <rohanjangid631@gmail.com>
2025-09-26 10:43:13 +0530

    updated for the all the route of the ECG

--- frontend/tat-dashboard/src/App.js
+++ frontend/tat-dashboard/src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DoctorDashboard from './pages/DoctorDashboard';
 import Viewer from './pages/Viewer'; // Create this file with "Hello World"
 import ClientDashboard from "./pages/client/ClientDashboard";
+import CoordinatorDashboard from "./pages/coordinator/CoordinatorDashboard";
+import ECGDashboard from "./pages/ecg/ECGDashboard";
+import ECGPatientDashboard from "./pages/ecg/ECGPatientDashboard";
+import ECGPDFDashboard from "./pages/ecg/ECGPDFDashboard";  
+
 
 function App() {
   return (
@@ -11,6 +16,10 @@ function App() {
         <Route path="/" element={<DoctorDashboard />} />
         <Route path="/viewer" element={<Viewer />} />
         <Route path="/client-dashboard" element={<ClientDashboard />} />
+        <Route path="/coordinator-dashboard" element={<CoordinatorDashboard />} />
+        <Route path="/ecg-dashboard" element={<ECGDashboard />} />
+        <Route path="/ecg-patient-dashboard" element={<ECGPatientDashboard />} />
+        <Route path="/ecg-pdf-dashboard" element={<ECGPDFDashboard />} />
       </Routes>
     </Router>
   );
